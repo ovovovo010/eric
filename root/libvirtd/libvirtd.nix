25cda69910267e1422b09fcf0ed5aff8fbd0d4bd
Eric <ovovovo010@proton.me>
2026-03-07 02:14:47 +0800

    fix

--- root/libvirtd/libvirtd.nix
+++ root/libvirtd/libvirtd.nix
@@ -1,22 +1,13 @@
-{ pkgs, config, lib, ... }:
-{
-  # 啟用 KVM/libvirt 虛擬化
+{ pkgs, ... }: {
   virtualisation.libvirtd = {
     enable = true;
 
     qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = false;
-      swtpm.enable = true;        # TPM 模擬（Windows 11 需要）
-      ovmf = {
-        enable = true;         # UEFI 韌體
-        packages = [ pkgs.OVMFFull.fd ]; # 含 Secure Boot 支援
-      };
+      package    = pkgs.qemu_kvm;
+      runAsRoot  = false;
+      swtpm.enable = true;   # TPM 模擬（Windows 11 需要）
     };
   };
 
-
-
-  # 讓 virtio 網路正常運作
   virtualisation.spiceUSBRedirection.enable = true;
 }
