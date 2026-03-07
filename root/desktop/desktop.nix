113323ef396c0a0d6f4fc8784d95268b53e8fdb0
Eric <ovovovo010@proton.me>
2026-03-07 11:55:13 +0800

    update

--- root/desktop/desktop.nix
+++ root/desktop/desktop.nix
@@ -1,7 +1,5 @@
 {pkgs, ...}: {
   services.xserver.enable = true;
   programs.hyprland.enable = true;
-  services.xserver.windowManager.icewm.enable = true;
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  programs.wayfire.enable = true;
 }
