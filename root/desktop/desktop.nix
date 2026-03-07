973a154d4790639cb7c4d5767276444615c79e5b
Eric <ovovovo010@proton.me>
2026-03-07 11:24:02 +0800

    fix

--- root/desktop/desktop.nix
+++ root/desktop/desktop.nix
@@ -1,11 +1,8 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{ pkgs, ... }: {
   services.xserver.enable = true;
   programs.hyprland.enable = true;
-  services.windowManager.openbox.enable = true;
-  services.windowManager.icewm.enable = true;
+  services.xserver.windowManager.openbox.enable = true;
+  services.xserver.windowManager.icewm.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 }
