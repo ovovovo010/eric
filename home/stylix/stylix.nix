88f49167b69ea1330cb91f277f608f4770c731d3
Eric <ovovovo010@proton.me>
2026-03-07 10:50:24 +0800

    fix

--- home/stylix/stylix.nix
+++ home/stylix/stylix.nix
@@ -1,7 +1,11 @@
 # /etc/nixos/home/stylix.nix
-{ config, pkgs, lib, inputs, ... }:
-
 {
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
   # 注意：系統層級 (root/stylix.nix) 已經設定了 stylix 主題和字體
   # 這裡只做 home-specific 的配置
 
@@ -22,10 +26,6 @@
   };
 
   # Kvantum 配置
-  home.file.".config/Kvantum/kvantum.kvconfig".text = ''
-    [General]
-    theme=Kvantum
-  '';
 
   # qt5ct 配置
   home.file.".config/qt5ct/qt5ct.conf".text = ''
@@ -33,11 +33,11 @@
     style=kvantum
     icon_theme=Papirus-Dark
     color_scheme=default
-    
+
     [Fonts]
     general=Noto Sans,10,-1,5,50,0,0,0,0,0
     fixed=JetBrainsMono Nerd Font Mono,10,-1,5,50,0,0,0,0,0
-    
+
     [Interface]
     dialogs=default
     menubar=default
