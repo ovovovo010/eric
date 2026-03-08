0a3cf403400550320d93926514d8d3fe669a7ae8
Eric <ovovovo010@proton.me>
2026-03-08 20:46:50 +0800

    fix

--- root/fcitx5/fcitx5.nix
+++ root/fcitx5/fcitx5.nix
@@ -1,5 +1,4 @@
 {pkgs, ...}: {
-  # 确保主题文件可访问
   environment.pathsToLink = ["/share/fcitx5"];
 
   i18n.inputMethod = {
@@ -12,11 +11,7 @@
       fcitx5-table-extra
       fcitx5-chewing
       fcitx5-lua
-      (catppuccin-fcitx5.override {
-        # 显式指定 flavor
-        flavor = "mocha";
-        accent = "lavender";
-      })
+      catppuccin-fcitx5
       fcitx5-rime
       rime-data
     ];
@@ -24,26 +19,7 @@
     fcitx5.settings = {
       addons = {
         classicui.globalSection = {
-          "Vertical Candidate List" = "True";
-          "WheelForPaging" = "True";
-          "Font" = "Sarasa Gothic TC 12";
-          "MenuFont" = "Sarasa Gothic TC 12";
-          "TrayFont" = "Sarasa Gothic TC Bold 10";
-          "TrayOutlineColor" = "#000000";
-          "TrayTextColor" = "#ffffff";
-          "PreferTextIcon" = "False";
-          "ShowLayoutNameInIcon" = "True";
-          "UseInputMethodLanguageToDisplayText" = "True";
-
-          # 修正主题名称 - 先试试这个
-          "Theme" = "catppuccin-mocha-lavender"; # 全小写
-          # 如果上面的不行，试试这个：
-          # "Theme" = "Catppuccin-Mocha";
-
-          "DarkTheme" = "catppuccin-mocha-lavender";
-          "UseDarkTheme" = "False";
-          "FollowSystemDarkMode" = "False";
-          "UseAccentColor" = "True";
+          "Theme" = "Catppuccin-Mocha"; # 试试这个
         };
       };
     };
