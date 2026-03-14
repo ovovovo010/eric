304c33f6b597a183e578cf1d88818ae195929cee
eric <ovovovo010@proton.me>
2026-03-15 07:27:56 +0800

    update

--- home/noctalia/noctalia.nix
+++ home/noctalia/noctalia.nix
@@ -3,9 +3,23 @@
     enable = true;
     settings = {
       bar = {
-        density = "compact";
         position = "top";
+        density = "compact";
+        floating = true;          # 漂浮感，不貼平螢幕邊緣
+        marginVertical = 10;      # 上方留空
+        marginHorizontal = 20;    # 左右縮短，長條狀
+        frameThickness = 6;
+        frameRadius = 16;         # 圓角
+        outerCorners = true;
+        backgroundOpacity = 0.85;
         showCapsule = true;
+        capsuleOpacity = 1.0;
+        fontScale = 1.3;          # 2K 放大字體
+        widgetSpacing = 8;
+        contentPadding = 4;
+        displayMode = "always_visible";
+        showOnWorkspaceSwitch = true;
+
         widgets = {
           left = [
             {
@@ -13,10 +27,7 @@
               useDistroLogo = true;
             }
             {
-              id = "Network";
-            }
-            {
-              id = "Bluetooth";
+              id = "ActiveWindow";
             }
           ];
           center = [
@@ -28,9 +39,23 @@
           ];
           right = [
             {
+              id = "Network";
+            }
+            {
+              id = "Bluetooth";
+            }
+            {
+              id = "Volume";
+            }
+            {
+              id = "Tray";
+            }
+            {
+              id = "NotificationHistory";
+            }
+            {
               id = "Clock";
               formatHorizontal = "HH:mm";
-              formatVertical = "HH mm";
               useMonospacedFont = true;
               usePrimaryColor = true;
             }
