dd6de4d15a247c7e4495f6a378fab1089acaeb29
eric <ovovovo010@proton.me>
2026-03-15 19:23:11 +0800

    [broken] 2 files changed, 20 insertions(+), 22 deletions(-)

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -22,15 +22,20 @@
         "fcitx5 -d"
         "noctalia-shell"
         "hyprctl setcursor catppuccin-mocha-lavender-cursors 24"
-        "linux-wallpaperengine --assets-dir /mnt/data/SteamLibrary/steamapps/common/wallpaper_engine/assets --screen-root DP-6 --volume 100 /mnt/data/SteamLibrary/steamapps/workshop/content/431960/3537678022"
+        "exec-once = linux-wallpaperengine --assets-dir /mnt/data/SteamLibrary/steamapps/common/wallpaper_engine/assets --screen-root DP-6 /mnt/data/SteamLibrary/steamapps/workshop/content/431960/3492627662"
+        "exec-once = ~/.config/hypr/scripts/wallpaper-audio.sh"
       ];
-
-      # WINDOWRULE
-      windowrulev2 = [
-      ];
-
-      # LOOK AND FEEL
-      general = {
+  windowrulev2 = float, class:^(linux-wallpaperengine)$
+  windowrulev2 = pin, class:^(linux-wallpaperengine)$
+  windowrulev2 = size 100% 100%, class:^(linux-wallpaperengine)$
+  windowrulev2 = move 0 0, class:^(linux-wallpaperengine)$
+  windowrulev2 = noborder, class:^(linux-wallpaperengine)$
+  windowrulev2 = noshadow, class:^(linux-wallpaperengine)$
+  windowrulev2 = noblur, class:^(linux-wallpaperengine)$
+  windowrulev2 = noanim, class:^(linux-wallpaperengine)$
+  windowrulev2 = nofocus, class:^(linux-wallpaperengine)$
+  windowrulev2 = immediate, class:^(linux-wallpaperengine)$
+        general = {
         gaps_in = 5;
         gaps_out = 20;
         border_size = 3;
--- home/noctalia/noctalia-shell.nix
+++ home/noctalia/noctalia-shell.nix
@@ -1,16 +1,9 @@
-{ lib, ... }:
 {
-  programs.noctalia-shell = {
-    enable = true;
-    settings = {
-      colorSchemes = {
-        darkMode = true;
-        useWallpaperColors = true;
-      };
-      bar = {
-        backgroundOpacity = lib.mkForce 0.8;
-        floating = true;
-      };
-    };
-  };
+  inputs,
+  pkgs,
+  ...
+}: {
+  home.packages = [
+    inputs.noctalia.packages.${pkgs.system}.default
+  ];
 }
