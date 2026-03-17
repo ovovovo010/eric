fcc73de974f34a5c7c45df4f9c92b2b764bc77f3
eric <ovovovo010@proton.me>
2026-03-17 19:28:19 +0800

    [fix] 1 file changed, 1 deletion(-)

--- home/hyprland/hyprland.nix
+++ home/hyprland/hyprland.nix
@@ -41,7 +41,6 @@ in {
         "match:class ^(linux-wallpaperengine)$, pin on"
         "match:class ^(linux-wallpaperengine)$, size 100% 100%"
         "match:class ^(linux-wallpaperengine)$, move 0 0"
-        "match:class ^(linux-wallpaperengine)$, noborder on"
         "match:class ^(linux-wallpaperengine)$, no_shadow on"
         "match:class ^(linux-wallpaperengine)$, no_blur on"
         "match:class ^(linux-wallpaperengine)$, no_anim on"
