30e1b5a7894297289097134b55ebc2fd96303627
eric <ovovovo010@proton.me>
2026-03-17 20:57:00 +0800

    [broken] 1 file changed, 1 deletion(-)

--- system/desktop/desktop.nix
+++ system/desktop/desktop.nix
@@ -3,7 +3,6 @@
     enable = true;
   };
   services.xserver.windowManager.i3 = true;
-  services.xserver.windowManager.bspwm = true;
   services.xserver.windowManager.dwm = true;
   programs.hyprland.enable = true;
 }
