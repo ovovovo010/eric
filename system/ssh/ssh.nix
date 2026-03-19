006b340dae9e2c01087b7f26925c265b9b6e5b73
eric <ovovovo010@proton.me>
2026-03-19 19:06:22 +0800

    [fix] 1 file changed, 1 insertion(+), 17 deletions(-)

--- system/ssh/ssh.nix
+++ system/ssh/ssh.nix
@@ -46,23 +46,7 @@
   };
 
   # ══════════════════════════════════════════════════════════════════════
-  #  2. initrd 階段 SSH（緊急模式可連線）
-  #     連線方式：ssh -p 2222 root@192.168.1.101
-  # ══════════════════════════════════════════════════════════════════════
-  boot.initrd.network = {
-    enable = true;
-    ssh = {
-      enable = true;
-      port = 2222;
-      authorizedKeys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILnY4J9PlO56cEMpTas6M/pRDj2bz3Lx2oMSPUieyCdk ovovovo010@proton.me"
-      ];
-      hostKeys = ["/etc/ssh/ssh_host_ed25519_key"];
-    };
-  };
-
-  # ══════════════════════════════════════════════════════════════════════
   #  3. 防火牆
   # ══════════════════════════════════════════════════════════════════════
-  networking.firewall.allowedTCPPorts = [22 2222];
+  networking.firewall.allowedTCPPorts = [22];
 }
